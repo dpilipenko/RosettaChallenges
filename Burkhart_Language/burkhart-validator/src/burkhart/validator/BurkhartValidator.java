a06c081918efcf28c62fae27183d0eb4c1195240
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2014-11-10 16:29:57 -0500

    Initial commit.

---
+++ Burkhart_Language/burkhart-validator/src/burkhart/validator/BurkhartValidator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package burkhart.validator;
+
+/**
+ * @author Dmitriy Pilipenko
+ *
+ */
+public class BurkhartValidator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Initial project setup. 
+		System.out.println("Validator.");
+	}
+
+}
