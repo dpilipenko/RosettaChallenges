287dfcad7b824b7b0262b2e06c7d2220be81d208
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2014-11-10 17:09:13 -0500

    include JUnit. Test for having at least one input argument

---
+++ Burkhart_Language/burkhart-validator/tests/burkhart/validator/BurkhartValidatorTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package burkhart.validator;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Dmitriy Pilipenko
+ *
+ */
+public class BurkhartValidatorTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void setUpStreams() {
+		 System.setOut(new PrintStream(outContent));
+		 System.setErr(new PrintStream(errContent));
+	}
+	
+	/**
+	 * Test Zero Input Arguments
+	 */
+	@Test
+	public void testNoInputArgument() {
+		BurkhartValidator.main(new String[] {});
+		assertEquals('0', outContent.toByteArray()[0]);
+	}
+	
+	/**
+	 * Test One Input Argument
+	 */
+	@Test
+	public void testInputArgument() {
+		BurkhartValidator.main(new String[] {"input.txt"});
+		assertEquals('1', outContent.toByteArray()[0]);
+	}
+	
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+		System.setErr(null);
+	}
+
+}
